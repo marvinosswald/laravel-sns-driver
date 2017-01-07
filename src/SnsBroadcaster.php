a92a2d53d6c7617bb7ac1d2df042347ecc6a40e7
Marvin Osswald <mail@marvinosswald.de>
2017-01-07 12:10:24 +0100

    added auth and validAuthenticationResponse function which are required by contract

--- src/SnsBroadcaster.php
+++ src/SnsBroadcaster.php
@@ -12,6 +12,16 @@ class SnsBroadcaster implements Broadcaster
 		$this->client = $client;
 	}
 
+	public function auth($request)
+    {
+        //
+    }
+
+    public function validAuthenticationResponse($request, $result)
+    {
+        //
+    }
+
 	public function broadcast(array $channels, $event, array $payload = array())
 	{
 		$payload = ['event' => $event, 'data' => $payload];
